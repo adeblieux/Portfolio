3030ac30f08fd717dfba54203b632e700c0aa2d8
A. DeBlieux <113622999+adeblieux@users.noreply.github.com>
2026-08-12 17:38:51 -0400

    Set base path for Vite configuration
    Signed-off-by: A. DeBlieux <113622999+adeblieux@users.noreply.github.com>

--- portfolio/vite.config.ts
+++ portfolio/vite.config.ts
@@ -4,6 +4,7 @@ import plugin from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
+    base: '/Portfolio/',
     server: {
         port: 65034,
     }
